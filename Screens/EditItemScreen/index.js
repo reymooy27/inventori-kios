685a1df0460c0220c7167d1696b7b6679dd8af1c
Rey Mooy <gdrrey@gmail.com>
2022-02-02 00:29:13 +0800

    update/submit changes

--- Screens/EditItemScreen/index.js
+++ Screens/EditItemScreen/index.js
@@ -1,9 +1,15 @@
-import React, { useLayoutEffect } from 'react';
-import {View,TextInput, StyleSheet} from 'react-native';
+import React, { useLayoutEffect, useState, useEffect } from 'react';
+import {Text,View,TextInput,TouchableOpacity, StyleSheet} from 'react-native';
+import { doc, updateDoc } from "firebase/firestore";
 
 const EditItemScreen = ({navigation, route}) => {
 
   const {item} = route.params
+
+  const [initialState, setInitialState] = useState({
+    productName: "",
+    productPrice: "",
+  });
 
   useLayoutEffect(()=>{
     navigation.setOptions({
@@ -12,16 +18,35 @@ const EditItemScreen = ({navigation, route}) => {
     });
   },[])
 
+  useEffect(() => {
+    setInitialState({
+      productName: item?.productName,
+      productPrice: item?.productPrice,
+    })
+  }, []);
+
+  const submit = async () =>{
+    const itemRef = doc(db, "products", item?.id);
+    await updateDoc(itemRef, initialState);
+  }
+
   return (
     <View style={{flex: 1, alignItems: "center"}}>
       <TextInput 
         style={{width: '90%', padding: 10, borderWidth: 2, borderColor: 'black', marginVertical: 10, borderRadius: 10}} 
-        value={item?.productName} 
+        value={initialState?.productName} 
+        onChangeText={(text)=> setInitialState({...initialState,...{productName: text}})}
         placeholder='Product Name'/>
       <TextInput 
         style={{width: '90%', padding: 10, borderWidth: 2, borderColor: 'black', marginVertical: 10, borderRadius: 10}} 
-        value={`${item?.productPrice}`} 
+        value={`${initialState?.productPrice}`} 
+        onChangeText={(text)=> setInitialState({...initialState,...{productPrice: text}})}
         placeholder='PRoduct Price' keyboardType='number-pad'/>
+        <TouchableOpacity onPress={submit}>
+          <Text>
+            Done
+          </Text>
+        </TouchableOpacity>
     </View>
   );
 }
