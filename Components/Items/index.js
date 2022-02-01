842f82088ff24a179e027bc4ee783ab6d4cd885e
Rey Mooy <gdrrey@gmail.com>
2022-02-02 00:28:04 +0800

    add price

--- Components/Items/index.js
+++ Components/Items/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
 
-const Items = ({name, onPress}) => {
+const Items = ({name,price, onPress}) => {
   return (
     <TouchableOpacity onPress={onPress}>
-      <View style={{width: '100%',height: 100, borderRadius: 15, 
+      <View style={{width: '100%',height: 50, borderRadius: 15, 
       padding: 10, backgroundColor: 'red',
-      justifyContent: 'center', alignItems: 'center',
+      justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center',
       marginVertical: 5,
       }}>
         <Text style={{color: 'black'}}>{name}</Text>
+        <Text style={{color: 'black'}}>{price}</Text>
       </View>
     </TouchableOpacity>
   );
